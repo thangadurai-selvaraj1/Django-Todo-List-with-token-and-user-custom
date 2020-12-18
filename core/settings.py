79655807c941326ab3323b7bdfed3a4bfddb3f24
Thangadurai Alvin <thangadurai@kuyatechnologies.com>
2020-12-18 19:08:29 +0530

    jwt time limit added

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import datetime
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -139,4 +139,14 @@ JWT_AUTH = {
     # # this is the maximum time AFTER the token was issued that
     # # it can be refreshed.  exprired tokens can't be refreshed.
     # 'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
+
+    # how long the original token is valid for
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=20),
+
+    # allow refreshing of tokens
+    'JWT_ALLOW_REFRESH': True,
+
+    # this is the maximum time AFTER the token was issued that
+    # it can be refreshed.  exprired tokens can't be refreshed.
+    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=70),
 }
